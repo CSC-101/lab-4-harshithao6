9696d4ed779a8d28f7eac201b9191c765502b2fe
Harshitha <hthimmap@calpoly.edu>
2024-10-24 22:23:07 -0700

    final project

--- lab4.py
+++ lab4.py
@@ -1,22 +1,70 @@
 import data
-
+import math
 # Write your functions for each part in the space below.
 
 # Part 1
+#
+def first_element(list: list[list[int]])-> list:
+    Innerlist = []
+    for i in range(len(list)):
+        if len(list[i]) > 0:
+            Innerlist.append(list[i][0])
+    return Innerlist
+
+
 
 
 # Part 2
+def x_coordinates(list: list[data.Point])->list:
+    new_list = []
+    for i in range(len(list)):
+        new_list.append(list[i].x)
+    return(new_list)
 
 
 # Part 3
 
+def are_in_positive_quadrant(list: list[data.Point]):
+    pos_list = []
+
+    for i in range(len(list)):
+        if(list[i].x>0 and list[i].y>0):
+            coorlist = []
+            coorlist.append(list[i].x)
+            coorlist.append(list[i].y)
+            pos_list.append(coorlist)
+    return(pos_list)
+
+
 
 # Part 4
+def distance(point1 = data.Point,point2 = data.Point)-> float:
+    x_distance = (point1.x-point2.x)**2
+    y_distance = (point1.y-point2.y)**2
+    return math.sqrt(x_distance+y_distance)
+
+
 
 
 # Part 5
+def manhattan_distance(p1 = data.Point,p2 = data.Point):
+    x_distance = abs(p1.x - p2.x)
+    y_distance = abs(p1.y - p2.y)
+    return(x_distance+y_distance)
+
+
 
 
 # Part 6
+
+def distance_all(list: list[data.Point]):
+    distance = []
+    for i in range(len(list)):
+        x_distance = (list[i].x - 0)**2
+        y_distance = (list[i].y - 0)**2
+        Dis_final = math.sqrt(x_distance+y_distance)
+        distance.append(Dis_final)
+    return distance
+
 
 
--- lab4_tests.py
+++ lab4_tests.py
@@ -1,6 +1,10 @@
+import math
+
 import data
 import lab4
 import unittest
+
+from lab4 import distance, manhattan_distance, distance_all
 
 
 # Write your test cases for each part below.
@@ -16,21 +20,59 @@ class TestCases(unittest.TestCase):
 
     def test_first_element_2(self):
         # write a second test here
+        input = [[2, 2], [5, 4]]
+        result = lab4.first_element(input)
+        expected = [2, 5]
+        self.assertEqual(expected, result)
+
+
 
 
     # Part 2
-
+    def test_x_coordinates(self):
+        point1 = data.Point(3, 4)
+        point2 = data.Point(5, 6)
+        input = [point1,point2]
+        result = lab4.x_coordinates(input)
+        expected = [3,5]
+        self.assertEqual(expected,result)
 
     # Part 3
+    def test_are_in_positive_quadrant(self):
+        point1 = data.Point(3, 4)
+        point2 = data.Point(5, 6)
+        input = [point1, point2]
+        result = lab4.are_in_positive_quadrant(input)
+        expected = [[3,4],[5,6]]
+        self.assertEqual(expected, result)
 
 
     # Part 4
+    def test_distance(self):
+        point1 = data.Point(3, 4)
+        point2 = data.Point(5, 6)
+        result = distance(point1,point2)
+        expected = math.sqrt(8)
+        self.assertEqual(expected, result)
 
 
     # Part 5
+    def test_manhattan_distance(self):
+        point1 = data.Point(3, 4)
+        point2 = data.Point(5, 6)
+        result = manhattan_distance(point1, point2)
+        expected = 4
+        self.assertEqual(expected, result)
 
 
     # Part 6
+    def test_distance_all(self):
+        inp = data.Point(4, 5)
+        inp2 = data.Point(4, 8)
+        List2 = [inp, inp2]
+        result = distance_all(List2)
+        expected = [math.sqrt(41),math.sqrt(80)]
+        self.assertEqual(expected, result)
 
 
 
